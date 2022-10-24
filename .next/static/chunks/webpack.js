a27fe466d849f11df54c85c9d9135b69fed5f2bd
Gustavo Holzmann <gustavobarretohv@live.com>
2022-10-24 12:47:07 -0300

    HTML do site iniciado

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "9e93fa84eb892902"; }
+/******/ 		__webpack_require__.h = function() { return "5c4183aeda3c7c4e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
@@ -1222,6 +1222,11 @@
 /******/ 		var chunkLoadingGlobal = self["webpackChunk_N_E"] = self["webpackChunk_N_E"] || [];
 /******/ 		chunkLoadingGlobal.forEach(webpackJsonpCallback.bind(null, 0));
 /******/ 		chunkLoadingGlobal.push = webpackJsonpCallback.bind(null, chunkLoadingGlobal.push.bind(chunkLoadingGlobal));
+/******/ 	}();
+/******/ 	
+/******/ 	/* webpack/runtime/nonce */
+/******/ 	!function() {
+/******/ 		__webpack_require__.nc = undefined;
 /******/ 	}();
 /******/ 	
 /************************************************************************/
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,19 @@
 import React from 'react';
-
+import Header from '../src/components/header/header';
+import BlocoTextoHome from '../src/components/blocoTextoHome/blocoTexto';
+import Footer from '../src/components/footer/footer';
 
 export default function Home(){
     return (
-        <div>Home</div>
+        <>
+            <div className='container'>
+                <Header/>
+                <div>Área Interativa</div>
+                <BlocoTextoHome titulo="Sobre"/>
+                <BlocoTextoHome titulo="Como?"/>
+                <BlocoTextoHome titulo="Para quem?"/>
+                <Footer/>
+            </div>
+        </>
     )
 }
