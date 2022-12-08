b335f331af99010d61d10e9cbebf4a8d2c915b20
Gustavo Holzmann <gustavobarretohv@live.com>
2022-12-08 18:45:23 -0300

    height bug fix

--- src/components/areaInterativa/interacoes/interacao1p5.tsx
+++ src/components/areaInterativa/interacoes/interacao1p5.tsx
@@ -1,17 +1,8 @@
 import p5Types from "p5";
 import dynamic from "next/dynamic";
 import styles from "./interacoes.module.css";
-import { useState } from "react";
-
-
 
 export default function InteracaoP5(){
-    // let x = 50;
-    // let y = 50;
-    // let selected = 1;
-    // let randomNumber1:number;
-    // let randomNumber2:number;
-    // let randomNumber3:number;
 
     let diametroMin = 250;
     let diametroMax = 1500;
@@ -19,6 +10,11 @@ export default function InteracaoP5(){
     let aceleracao = 0.15;
     let xx,yy: number;
     let angulo = 0;
+
+    if((screen.availWidth)<1100){
+        diametroStep = 25;
+        diametroMax = 750;
+    }
   
     // Para prevenir erros de importação do React ao importar o `react-p5`
     const Sketch = dynamic(() => import('react-p5').then((mod) => mod.default), {
@@ -59,40 +55,6 @@ export default function InteracaoP5(){
         }  
     }
 
-     //Interação 1
-    // const draw = (p5: p5Types) => {
-
-    //     randomNumber1 = p5.random(0,p5.width);
-    //     randomNumber2 = p5.random(0,p5.height);
-    //     randomNumber3 = p5.random(0,25);
-
-    //     if(selected === 1){
-    //         p5.noStroke();
-    //         p5.fill(21,21,37,p5.random(255));
-    //         p5.ellipse(p5.random(p5.width),p5.random(p5.height), p5.random(15), p5.random(15));
-    //     }
-
-    //     if(selected ===2){
-    //         p5.noStroke();
-    //         p5.fill(21,21,37,p5.random(255));
-    //         p5.square(randomNumber1,randomNumber2,randomNumber3);
-    //     }
-
-    //     if(selected ===3){
-    //         p5.strokeWeight(randomNumber3/1.3);
-    //         p5.stroke('#FFFDF3');
-    //         p5.point(randomNumber1,randomNumber2);
-    //     }
-        
-
-    //     if(p5.mouseIsPressed){
-    //         selected+=1;
-    //         if(selected>3){
-    //             selected = 1;
-    //         } 
-    //     }
-
-	// };
     const conecto = `{Co.necte-se}`;
 
     return (
