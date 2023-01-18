1ea40c431b1a47ab0b0c9551fc64e887e967b4f0
Gustavo Holzmann <gustavobarretohv@live.com>
2023-01-18 16:06:01 -0300

    path bug fix

--- src/components/conteudo/conteudoInteracoes.tsx
+++ src/components/conteudo/conteudoInteracoes.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import styles from './Conteudo.module.css'
-import style from "../conteudo/postInteracoes/postInteracao.module.css";
+//import style from "../conteudo/postInteracoes/postInteracao.module.css";
 import SearchField from './searchField';
 import { drops } from './drops';
 
@@ -8,31 +8,30 @@ export default function ConteudoInteracoes(props:{tamanhoFonte:number,darkMode:b
 
     const [searchBox,setSearchBox] = useState("");
 
-    //Post
-    let container = `${style.containerPost}`
-    let titulo2 = `${style.titulo}`
-    let subtitulo = `${style.subtitulo}`
-    let data = `${style.data}`
-    let texto = `${style.texto}`
+    let container = `${styles.containerPost}`
+    let titulo2 = `${styles.titulo}`
+    let subtitulo = `${styles.subtitulo}`
+    let data = `${styles.data}`
+    let texto = `${styles.texto}`
     let containerPosts = `${styles.containerPosts}`
     let titulo = `${styles.titulo}`
     let descricao = `${styles.descricao}`
 
     if(props.darkMode === true){
-        container = `${style.containerPostBlack}`
-        titulo2 = `${style.tituloBlack}`;
-        subtitulo = `${style.subtituloBlack}`;
-        data = `${style.dataBlack}`;
-        texto = `${style.textoBlack}`
+        container = `${styles.containerPostBlack}`
+        titulo2 = `${styles.tituloBlack2}`;
+        subtitulo = `${styles.subtituloBlack}`;
+        data = `${styles.dataBlack}`;
+        texto = `${styles.textoBlack}`
         containerPosts = `${styles.containerPostsBlack}`
         titulo = `${styles.tituloBlack}`
         descricao = `${styles.descricaoBlack}`
     }else{
-        container = `${style.containerPost}`
-        titulo2 = `${style.titulo}`
-        subtitulo = `${style.subtitulo}`
-        data = `${style.data}`
-        data = `${style.data}`
+        container = `${styles.containerPost}`
+        titulo2 = `${styles.titulo2}`
+        subtitulo = `${styles.subtitulo}`
+        data = `${styles.data}`
+        data = `${styles.data}`
         containerPosts = `${styles.containerPosts}`
         titulo = `${styles.titulo}`
         descricao = `${styles.descricao}`
@@ -63,7 +62,7 @@ export default function ConteudoInteracoes(props:{tamanhoFonte:number,darkMode:b
 
     return (
         <div className={containerPosts}>
-            <h1 style={{ fontSize: `${props.tamanhoFonte+32}px` }} className={titulo}>Interações</h1>
+            <h1 style={{ fontSize: `${props.tamanhoFonte+32}px` }} className={titulo2}>Interações</h1>
             <h3 style={{ fontSize: `${props.tamanhoFonte}px` }} className={descricao}>Ao longo dos meses, desenvolvemos múltiplas interações, que, a partir do uso de determinada tecnologia, criam uma experiência única
                 para com o usuário.
             </h3>
@@ -80,11 +79,11 @@ export default function ConteudoInteracoes(props:{tamanhoFonte:number,darkMode:b
                 }
             }).map((post, index) => (
                 <div className={container} key={index}>
-                    <img className={style.img} src={`images/${post.srcImg}.png`}/>
-                    <div className={style.containerTexto}>
+                    <img className={styles.img} src={`images/${post.srcImg}.png`}/>
+                    <div className={styles.containerTexto}>
                         <h2 
                             style={{ fontSize: `${props.tamanhoFonte+28}px` }}
-                            className={titulo2}>
+                            className={titulo}>
                             {post.titulo}
                         </h2>
                         <h3
