fd3e8670bd60315c0817a60a70e1cd300a4cc8ab
Gustavo Holzmann <gustavobarretohv@live.com>
2023-01-26 15:26:17 -0300

    Texto alt adicionado as imagens

--- src/components/conteudo/drops.tsx
+++ src/components/conteudo/drops.tsx
@@ -7,7 +7,8 @@ export const drops = [
         data: '17/10/22',
         srcImg: "drop1telas",
         texto: "A areia, que se disperça facilmente com o vento,foi o elemento simbólico da interação, utilizando-se da silhueta de uma pessoa como referência para possibilitar uma ação rativa com as fotografias que estão em outra camada. Esse contato, tem como significado representativo, de que a partir da presença de uma pessoa, é possível fazer um recorte no tempo e na memória, e na medida em que esse corpo ocupa esse espaço, ele segmenta a existência e a memória da vila dos pescadores.",
-        textoAdicional: "Principal artigo que fundamentou a proposta: ieeexplore.ieee.org/document/ 8603589"                  
+        textoAdicional: "Principal artigo que fundamentou a proposta: ieeexplore.ieee.org/document/ 8603589",
+        textoDescritivoImagem: "Telão de LED sendo utilizado em interação com o aparelho Kinect"                  
     },
     {
         index:1,
@@ -16,7 +17,8 @@ export const drops = [
         data: '15/10/22',
         srcImg: "arduino",
         texto: "O uso de placas de arduino e seus módulos possuem diversas utilidades, desde lâmpadas inteligentes até controladores de irrigação para jardinagem. O seu uso foi muito importante para que fosse possível alcançar o resultado desejado nos experimentos, mas mais importante que isso, criar uma ferramenta que pudesse suportar outras propostas. Para isso, foi usado dois módulos de radiofrequência (RFID RC522), conectados a uma placa de Arduino Uno, que conseguem ler quando um cartão de radiofrequência se aproxima, identificar qual cartão foi lido e qual módulo o leu, mandando a informação para outros softwares, como o TouchDesigner para mudar a imagem em uma projeção, como foi usado, ao utilizar um desses cartões dentro de um impresso 3D.",                  
-        textoAdicional: "Essa proposta de interação surgiu a partir de sua utilização no Laboratório de Experiência Digital (LED)"        
+        textoAdicional: "Essa proposta de interação surgiu a partir de sua utilização no Laboratório de Experiência Digital (LED)",
+        textoDescritivoImagem: "Placa de Hardware Arduino UNO"        
     },
     {
         index:2,
@@ -25,6 +27,7 @@ export const drops = [
         data:'15/09/22',
         srcImg:"concept",
         texto:"A criação de uma narrativa para guiar o processo de desenvolvimento das nossas instalações e experiências foi um ponto importante do nosso processo. Para isso, essa história precisava ser: Baseada em um contexto e uma problemática real, para que as experiências pudessem oferecer reflexões relevantes; Simples e abstrata, para que pudesse dar origem a muitas ideias sem fortes limitações lógicas ou estéticas. Assim, resolvemos explorar o contexto e a cultura da vila de pescadores do Mucuripe (Fortaleza - CE), explorando a sua história e a manutenção da sua memória ameaçada pela especulação imobiliária. A história foi então dividida em 7 partes e acompanha um pescador e a sua filha, intermediada pela lenda de um peixe místico.",
-        textoAdicional: "Nosso cinegrafista coletou registros na vila, que podem ser encontrados no nosso Instagram",      
+        textoAdicional: "Nosso cinegrafista coletou registros na vila, que podem ser encontrados no nosso Instagram",
+        textoDescritivoImagem: "Concept art de personagem criado a partir da vila do mucuripe" 
     },
 ];
